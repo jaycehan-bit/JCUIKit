cca99bd774140a6ff940208f5f6b0d21ddc466b0
jaycehan <jaycehan@tencent.com>
2024-05-20 10:41:11 +0800

    Initial Commit

---
+++ JCUIStorageUITests/JCUIStorageUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  JCUIStorageUITestsLaunchTests.swift
+//  JCUIStorageUITests
+//
+//  Created by jaycehan on 2024/5/20.
+//
+
+import XCTest
+
+final class JCUIStorageUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
